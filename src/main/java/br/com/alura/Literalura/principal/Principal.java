7e710b098968fde6c5c7d75699b63b2cb7baafa7
Giovanni Morais <giovannimo1@yahoo.com>
2024-11-04 18:17:30 -0300

    feat: Consumindo API

--- src/main/java/br/com/alura/Literalura/principal/Principal.java
+++ src/main/java/br/com/alura/Literalura/principal/Principal.java
@@ -1,4 +1,13 @@
 package br.com.alura.Literalura.principal;
 
+import br.com.alura.Literalura.service.ConsumoApi;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private final String ENDERECO = "https://gutendex.com/books?search="; //nome do livro aqui;
 }
